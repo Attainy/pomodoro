4f300217edd1a54706313bfcf4d683fdb54441a6
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-28 00:52:20 +0900

    눈금 완성

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /* 시계 눈금 */
-const clockBorder = document.querySelector('.clock__border');
+// const scaleWidth = clockBorder.clientWidth / 2;
+// const clockScaleDiv = document.querySelector('.clock__scale');
+const clockScaleDiv = document.querySelector('.clock__circle');
+
 
 for (let i=0; i<60; i++) {
   let clockScale = document.createElement('div');
-  clockScale.classList = 'clock__scale';
+  clockScale.classList = 'clock__scale-line';
 
-  clockBorder.appendChild(clockScale);
-  clockScale.style = `transform-origin: bottom; transform: rotate(${6 * i}deg);`;
+  clockScaleDiv.appendChild(clockScale);
+  clockScale.style = `transform: rotate(${6 * i}deg); z-index: ${i%5 === 0 ? 2 : 0}`;
 }
+
+
